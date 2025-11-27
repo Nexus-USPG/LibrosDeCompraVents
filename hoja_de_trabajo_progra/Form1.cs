7122710f4b8c0eeaea0819dd47bef0fcb9772ee3
Betza <betza.otaku.karla@gmail.com>
2025-11-26 22:05:28 -0600

    nuevas actualizaciones

--- hoja_de_trabajo_progra/Form1.cs
+++ hoja_de_trabajo_progra/Form1.cs
@@ -34,6 +34,34 @@ namespace hoja_de_trabajo_progra
         private void button2_Click(object sender, EventArgs e)
         {
             string bus = textBox2.Text;
+            string sql ="SELECT * FROM clientes WHERE Nit='" + bus + "'";
+            List<string> lista = new List<string>();
+            if(!string.IsNullOrEmpty(bus))
+            {
+                Conexion con = new Conexion();
+                SqlCommand cmd = new SqlCommand(sql, con.ObtenerConexion());
+                SqlDataReader reader = cmd.ExecuteReader();
+                if(reader.HasRows)
+                {
+                    while(reader.Read())
+                    {
+                        lista.Add(reader.GetString(0));
+                        lista.Add(reader.GetString(1));
+                        lista.Add(reader.GetString(2));
+                    }
+                    textBox1.Text = lista[1];
+                    textBox2.Text = lista[0];
+                    textBox3.Text = lista[2];
+                }
+                else
+                {
+                    MessageBox.Show("No se encontraron registros");
+                }
+            }
+            else
+            {
+                MessageBox.Show("Ingrese un Nit valido");
+            }
 
         }
 
