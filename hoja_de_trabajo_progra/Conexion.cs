9e11f83dcac028a186da843fc1c40d6cbb1a81db
Betza <betza.otaku.karla@gmail.com>
2025-11-26 21:40:45 -0600

    Prueba de conexion

---
+++ hoja_de_trabajo_progra/Conexion.cs
@@ -0,0 +1,43 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace hoja_de_trabajo_progra
+{
+    class Conexion
+    {
+        private MySqlConnection cnx;
+
+        public Conexion()
+        {
+            try
+            {
+                cnx = new MySqlConnection("Server=localhost;Port=3306;Database=libro_compras_ventas;Uid=root;Pwd=SpikeOso;SslMode=Required;TlsVersion=Tls12,Tls13;");
+
+                cnx.Open();
+
+                MessageBox.Show("Conexion Exitosa", "Si compila", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            catch (MySqlException mysqlEx)
+            {
+                MessageBox.Show("Error de Conexion: " + mysqlEx.Message, "Error de MySQL", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error de Conexion: " + ex.Message, "Error Humano", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+        public MySqlConnection ObtenerConexion()
+        {
+            return cnx;
+        }
+    }
+
+         
+    }        
+ 
---
+++ hoja_de_trabajo_progra/Form1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace hoja_de_trabajo_progra
+{
+    
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string nombre = textBox1.Text;
+            string Nit = textBox2.Text;
+            int telefono = int.Parse(textBox3.Text);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string bus = textBox2.Text;
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Conexion con = new Conexion();
+        }
+    }
+}
