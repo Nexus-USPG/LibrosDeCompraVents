d058a18ac0e1103c9cfbdc7f93f9ee0e891ff70e
Dallin Osorio <dallin.osorio@outlook.com>
2025-11-26 22:29:29 -0600

    Se agregan clases Cliente y Factura

---
+++ hoja_de_trabajo_progra/Dominio/Cliente.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace hoja_de_trabajo_progra.Dominio
+{
+    public class Cliente
+    {
+        public int Id { get; set; } = Guid.NewGuid().GetHashCode();
+        public string NIT { get; set; } = "C/F";
+        public string Nombre { get; set; }
+
+        public string Telefono { get; set; }
+        public Cliente(int id, string nit, string nombre, string telefono)
+        {
+            Id = id;
+            NIT = nit;
+            Nombre = nombre;
+            Telefono = telefono;
+        }
+    }
+}
---
+++ hoja_de_trabajo_progra/Dominio/Factura.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace hoja_de_trabajo_progra.Dominio
+{
+    public class Factura
+    {
+        public string Tipo { get; set; } = "";
+
+        public DateTime FechaEmision { get; set; } = DateTime.Now;
+
+        public string Serie { get; set; } = "";
+        public string NumeroDTE { get; set; } = "";
+
+        public string NumeroAutorizacion { get; set; } = "";
+        public string NumeroAcceso { get; set; } = "";
+
+        public string Moneda { get; set; } = "GTQ";
+        public string NITEmisor { get; set; } = "";
+        public string NombreEmisor { get; set; } = "";
+        public string DireccionEmisor { get; set; } = "";
+        public string NITReceptor { get; set; } = "";
+        public string NombreReceptor { get; set; } = "";
+        public decimal TotalQ { get; set; } = 0m;
+        public decimal Impuestos { get; set; } = 0m;
+        public decimal Descuentos { get; set; } = 0m;
+        public string FormaPago { get; set; } = "";
+    }
+}
